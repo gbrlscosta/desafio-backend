a717dd9d6e8dece7d48c3464a9ccd70fd9c3f47b
gbrlscosta <gabriel.costa@germinare.org.br>
2024-05-23 15:40:02 -0300

    resolvendo problema de criação do usuario e de transferencia

--- app.py
+++ app.py
@@ -15,39 +15,58 @@ collection = db['clients']
 
 app = Flask(__name__)
 
+class User:
+    def __init__(self, id, nome_completo, cpf, email, saldo, senha, tipo):
+        self.id = id
+        self.nome_completo = nome_completo
+        self.cpf = cpf
+        self.email = email
+        self.saldo = saldo
+        self.senha = senha
+        self.tipo = tipo
+
+    def __repr__(self):
+        return (f"User(id={self.id}, nome_completo='{self.nome_completo}', cpf='{self.cpf}', "
+                f"email='{self.email}', saldo={self.saldo}, senha='{self.senha}', tipo='{self.tipo}')")
+
 @app.route("/dbpicpay", methods=["GET"])
 def read():
     try:
-        output = [
-            data for data in collection.find({}, {'_id': 0})
-        ]
+        output = list(collection.find({}, {'_id': 0}))
         return jsonify(output), 200
     except Exception as e:
         return jsonify({"Erro": str(e)}), 500
 
+
+
 @app.route("/dbpicpay", methods=["POST"])
 def create_user():
-    data = request.json
-    cpf = data.get('cpf')
-    email = data.get('email')
-    user_id = data.get('id')
-
-    for i in collection.find().sort({'id': -1}).limit(1):
-        if user_id != int(i['id']) + 1:
-            return jsonify({"Erro": f"ID Inválido!"}), 400
-
-    campos_necessarios = ['id', 'nome_completo', 'cpf', 'email', 'saldo', 'senha', 'tipo']
-
-    for campo in campos_necessarios:
-        if campo not in data:
-            return jsonify({"Erro": f"Campo {campo} é obrigatório!"}), 400
-
     try:
-        if collection.find_one({'$or': [{'cpf': cpf}, {'email': email}, {'id': user_id}]}):
+        data = request.json
+        user_obj = user_validation(data)
+
+        last_id = collection.find_one(sort=[('id', -1)])
+        id_correto = last_id['id'] + 1 if last_id else 1
+
+        if user_obj.id != id_correto:
+            return jsonify({"Erro": f"ID Inválido! Esperado: {id_correto}"}), 400
+
+        if collection.find_one({'$or': [{'cpf': user_obj.cpf}, {'email': user_obj.email}, {'id': user_obj.id}]}):
             return jsonify({"Erro": "Cliente Existente!"}), 400
 
-        collection.insert_one(data)
-        return jsonify({"Status": "Cliente Inserido!"}), 200
+        user = {
+            'id': user_obj.id,
+            'nome_completo': user_obj.nome_completo,
+            'cpf': user_obj.cpf,
+            'email': user_obj.email,
+            'saldo': user_obj.saldo,
+            'senha': user_obj.senha,
+            'tipo': user_obj.tipo
+        }
+
+        collection.insert_one(user)
+        return jsonify({"Status": "Cliente Inserido!"}), 201
+
     except Exception as e:
         return jsonify({"Erro": str(e)}), 500
 
@@ -61,42 +80,32 @@ def transfer():
     if not value or not id_payer or not id_payee:
         return jsonify({"Erro": "Valor, pagador e recebedor são obrigatórios!"}), 400
 
-    if validation_antifraud == True:
+    if not validation_antifraud(id_payee, id_payer):
+        return jsonify({"Erro": "Fraude detectada!"}), 403
 
-        if value <= 0:
-            return jsonify({"Erro": "O valor da transferência deve ser maior que zero!"}), 400
+    payer = collection.find_one({'id': id_payer})
+    payee = collection.find_one({'id': id_payee})
 
-        try:
-            payer = collection.find_one({'id': id_payer})
-            payee = collection.find_one({'id': id_payee})
+    if not payer or not payee or payer['tipo'] == 'lojista':
+        return jsonify({"Erro": "Transação inválida!"}), 403
 
-            if payer is None or payee is None:
-                return jsonify({"Erro": "Pagador ou recebedor não encontrados"}), 404
+    if payer['saldo'] < value:
+        return jsonify({"Erro": "Saldo Insuficiente!"}), 400
 
-            if payer['tipo'] == 'lojista':
-                return jsonify({'Erro': 'O Usuário não pode fazer transferência!'}), 403
+    saldo_payer = payer['saldo']
+    saldo_payee = payee['saldo']
 
-            saldo_payer = float(payer['saldo'])
-            saldo_payee = float(payee['saldo'])
+    try:
 
-            if saldo_payer < value:
-                return jsonify({"Erro": "Saldo Insuficiente!"}), 400
+        collection.update_one({'id': id_payer}, {'$inc': {'saldo': -value}})
+        collection.update_one({'id': id_payee}, {'$inc': {'saldo': value}})
+        send_email(value, payer, payee)
+        return jsonify({'Status': 'Transferência Concluída!'}), 200
+    except Exception as e:
+        collection.update_one({'id': id_payer}, {'$set': {'saldo': saldo_payer}})
+        collection.update_one({'id': id_payee}, {'$set': {'saldo': saldo_payee}})
+        return jsonify({"Erro": f"Falha na transferência!"}), 500
 
-            new_saldo_payer = saldo_payer - value
-            new_saldo_payee = saldo_payee + value
-
-            collection.update_one({'id': id_payer}, {'$set': {'saldo': new_saldo_payer}})
-            collection.update_one({'id': id_payee}, {'$set': {'saldo': new_saldo_payee}})
-
-            send_email(value, payer, payee)
-
-            return jsonify({'Status': 'Transferência Concluída!'}), 200
-        except Exception as e:
-            collection.update_one({'id': id_payer}, {'$set': {'saldo': saldo_payer}})
-            collection.update_one({'id': id_payee}, {'$set': {'saldo': saldo_payee}})
-            return jsonify({"Erro": str(e)}), 500
-    else:
-        return jsonify({"Erro": "Fraude!"}), 403
 
 def send_email(value, payer, payee):
     try:
@@ -118,8 +127,50 @@ def send_email(value, payer, payee):
     except Exception as e:
         print(f"Falha ao enviar email: {str(e)}")
 
-def validation_antifraud(payee, payer):
+def validation_antifraud(payee_id, payer_id):
     return True
+
+def user_validation(data):
+    campos_necessario = ['id', 'nome_completo', 'cpf', 'email', 'saldo', 'senha', 'tipo']
+
+    if not all(campos in data for campos in campos_necessario):
+        raise ValueError(f"está faltando campos.")
+
+    if not isinstance(data['id'], int):
+        raise ValueError("O campo 'id' deve ser um inteiro.")
+    if not isinstance(data['nome_completo'], str):
+        raise ValueError("O campo 'nome_completo' deve ser uma string.")
+    if not isinstance(data['cpf'], str):
+        raise ValueError("O campo 'cpf' deve ser uma string.")
+    if not isinstance(data['email'], str):
+        raise ValueError("O campo 'email' deve ser uma string.")
+    if not isinstance(data['saldo'], (int, float)):
+        raise ValueError("O campo 'saldo' deve ser um número.")
+    if not isinstance(data['senha'], str):
+        raise ValueError("O campo 'senha' deve ser uma string.")
+    if not isinstance(data['tipo'], str):
+        raise ValueError("O campo 'tipo' deve ser uma string.")
+
+    required_keys = ['id', 'nome_completo', 'cpf', 'email', 'saldo', 'senha', 'tipo']
+
+    if not all(key in data for key in required_keys):
+        raise ValueError("falta campos necessários.")
+
+    return User(
+        id=data['id'],
+        nome_completo=data['nome_completo'],
+        cpf=data['cpf'],
+        email=data['email'],
+        saldo=data['saldo'],
+        senha=data['senha'],
+        tipo=data['tipo']
+    )
+
+
 
 if __name__ == "__main__":
     app.run(debug=True, port=5005, host="0.0.0.0")
+
+
+
+
